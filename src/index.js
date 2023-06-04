2c976bc53196a2ed56199258eca53ac229707630
AdrianChudobinski <107555917+AdrianChudobinski@users.noreply.github.com>
2023-06-04 17:20:03 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import axios from 'axios';
 
 const form = document.querySelector('.search-form');
 const gallery = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more');
 
-import axios from 'axios';
+let currentPage = 1;
 
 form.addEventListener('submit', e => {
   e.preventDefault();
+  currentPage = 1;
 
   const q = document.querySelector('[name="searchQuery"]').value;
   console.log(q);
@@ -15,21 +17,37 @@ form.addEventListener('submit', e => {
   async function getPhotos() {
     try {
       const apiKey = '37018650-559b86afe30fd9216457fcff9';
-      const apiUrl = `https://pixabay.com/api/?key=${apiKey}&q=${q}&per_page=40`;
+      const apiUrl = `https://pixabay.com/api/?key=${apiKey}&q=${q}&page=${currentPage}&per_page=40`;
 
       const response = await axios.get(apiUrl);
       const photos = response.data.hits;
 
       console.log(photos);
 
+      if (photos.length === 0) {
+        Notify.failure('Brak wyników wyszukiwania');
+      }
+
       photos.forEach(photo => {
         const image = document.createElement('img');
         image.src = photo.webformatURL;
         gallery.appendChild(image);
       });
+
+      if (currentPage === 1) {
+        loadMoreBtn.style.display = 'block';
+      }
     } catch (error) {
       Notify.failure(error);
     }
   }
+
+  gallery.innerHTML = '';
+  loadMoreBtn.style.display = 'none';
+  getPhotos();
+});
+
+loadMoreBtn.addEventListener('click', () => {
+  currentPage++;
   getPhotos();
 });
