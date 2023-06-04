528c5ab594a8b3c404d1b55f848ff99ed327cdc7
AdrianChudobinski <107555917+AdrianChudobinski@users.noreply.github.com>
2023-06-04 19:46:53 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,48 +6,71 @@ const gallery = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more');
 
 let currentPage = 1;
+let currentQuery = '';
 
-form.addEventListener('submit', e => {
+form.addEventListener('submit', async e => {
   e.preventDefault();
-  currentPage = 1;
 
   const q = document.querySelector('[name="searchQuery"]').value;
   console.log(q);
 
-  async function getPhotos() {
-    try {
-      const apiKey = '37018650-559b86afe30fd9216457fcff9';
-      const apiUrl = `https://pixabay.com/api/?key=${apiKey}&q=${q}&page=${currentPage}&per_page=40`;
+  try {
+    const apiKey = '37018650-559b86afe30fd9216457fcff9';
+    const apiUrl = `https://pixabay.com/api/?key=${apiKey}&q=${q}&per_page=40`;
 
-      const response = await axios.get(apiUrl);
-      const photos = response.data.hits;
+    currentQuery = q;
+    currentPage = 1;
 
-      console.log(photos);
+    const response = await axios.get(apiUrl);
+    const photos = response.data.hits;
 
-      if (photos.length === 0) {
-        Notify.failure('Brak wyników wyszukiwania');
-      }
+    console.log(photos);
 
+    // Wyczyść galerię przed dodaniem nowych zdjęć
+    gallery.innerHTML = '';
+
+    if (photos.length === 0) {
+      Notify.failure('Brak wyników wyszukiwania');
+      loadMoreBtn.style.display = 'none'; // Ukryj przycisk "load more"
+    } else {
+      // Przetwarzanie i wyświetlanie zdjęć na stronie
       photos.forEach(photo => {
         const image = document.createElement('img');
         image.src = photo.webformatURL;
         gallery.appendChild(image);
       });
 
-      if (currentPage === 1) {
-        loadMoreBtn.style.display = 'block';
-      }
-    } catch (error) {
-      Notify.failure(error);
+      loadMoreBtn.style.display = 'block'; // Pokaż przycisk "load more"
     }
+  } catch (error) {
+    Notify.failure(error);
   }
-
-  gallery.innerHTML = '';
-  loadMoreBtn.style.display = 'none';
-  getPhotos();
 });
 
-loadMoreBtn.addEventListener('click', () => {
-  currentPage++;
-  getPhotos();
+loadMoreBtn.addEventListener('click', async () => {
+  try {
+    const apiKey = '37018650-559b86afe30fd9216457fcff9';
+    const apiUrl = `https://pixabay.com/api/?key=${apiKey}&q=${currentQuery}&per_page=40&page=${
+      currentPage + 1
+    }`;
+
+    const response = await axios.get(apiUrl);
+    const photos = response.data.hits;
+
+    if (photos.length === 0) {
+      Notify.failure('Brak kolejnych wyników');
+      loadMoreBtn.style.display = 'none'; // Ukryj przycisk "load more"
+    } else {
+      // Dodawanie kolejnych zdjęć do galerii
+      photos.forEach(photo => {
+        const image = document.createElement('img');
+        image.src = photo.webformatURL;
+        gallery.appendChild(image);
+      });
+
+      currentPage += 1;
+    }
+  } catch (error) {
+    Notify.failure(error);
+  }
 });
